e3d9280eb8ae1ede6c0c3c2f28266fcd8467fb5d
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-05-07 18:27:19 -0300

    criando tamplates

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const path = require('path')
 
 const basePath = path.join(__dirname, 'templates')
 
+app.get('/users/:id',(req, res)=> {
+    const id = req.params.id
+
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
 app.get('/', (req, res) => {
     res.send('Olá mundo!')
 })
